393e99d539cdf06481105f9ce16d1d740e567653
Teddy <teddy@jhnet.co.za>
2023-05-16 11:55:35 +0200

    Course results for the quiz app welcome screen

--- adv_dasics/lib/main.dart
+++ adv_dasics/lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:adv_dasics/welcome_screen/welcompage.dart';
+import 'package:adv_dasics/start_screen.dart';
 
 void main() {
-  runApp(const WelcomePage());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 82, 5, 153),
+                Color.fromARGB(255, 82, 4, 92),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
