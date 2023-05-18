7cbd3500f540a7133b1fb9716b70e65c69ed1624
Teddy <teddy@jhnet.co.za>
2023-05-18 11:36:49 +0200

    My attempt at adding the new stateful screens

---
+++ adv_dasics/lib/quiz.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _Quiz();
+  }
+}
+
+class _Quiz extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return const Text("Quiz");
+  }
+}
