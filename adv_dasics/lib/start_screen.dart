275717f426f7a194fc65eb81e4e70971b62b14fc
Teddy <teddy@jhnet.co.za>
2023-05-18 11:35:42 +0200

    Added icon to button and opacity to image

--- adv_dasics/lib/start_screen.dart
+++ adv_dasics/lib/start_screen.dart
@@ -12,6 +12,7 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             'assets/images/quiz-logo.png',
             height: 300,
+            color: const Color.fromARGB(158, 252, 251, 251),
           ),
           const SizedBox(height: 80),
           const Text(
@@ -22,12 +23,13 @@ class StartScreen extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 30),
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
-            child: const Text("start Quiz"),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text("start Quiz"),
           ),
         ],
       ),
