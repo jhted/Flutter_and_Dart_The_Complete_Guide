a3769389c91b7ff70348224bd8cede384b91bcbc
Teddy <teddy@jhnet.co.za>
2023-06-01 08:57:03 +0200

    Added the category page to the meals app

---
+++ meals/lib/screens/categories.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'package:meals/data/dummy_data.dart';
+
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("pick you category"),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(22),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+            ),
+        ],
+      ),
+    );
+  }
+}
